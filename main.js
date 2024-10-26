ff710a38e037d2fd807fda5b9988b5dae1c5345d
Mayyan Maria <mariapalees@gmail.com>
2024-10-26 13:27:30 +0300

    Implement onBallClick()

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 'use strict'
 
+document.querySelector('.ball').onclick = onBallClick;
+
+// הצגת הקוטר הראשוני של הכדור
+document.querySelector('.ball').textContent = '100'
+
 function onBallClick() {
-    console.log('hello')
+    var ball = document.querySelector('.ball')
     
+    // קבלת רוחב הכדור הנוכחי בעזרת getComputedStyle
+    var currentSize = parseInt(getComputedStyle(ball).width)
+    
+    var newSize = currentSize + 50
+    ball.style.width = newSize + 'px'
+    ball.style.height = newSize + 'px'
+    ball.textContent = newSize // עדכון הטקסט
 }
