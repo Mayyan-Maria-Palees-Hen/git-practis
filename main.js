1188c6be9977af6e68dccdc173ddba61129ddbef
Mayyan Maria <mariapalees@gmail.com>
2024-10-26 19:06:41 +0300

    Add another ball and styling

--- main.js
+++ main.js
@@ -1,29 +1,44 @@
 'use strict'
 
-document.querySelector('.ball').onclick = onBallClick
 
-// הצגת הקוטר הראשוני של הכדור
+// הצגת הקוטר הראשוני של הכדורים
 document.querySelector('.ball').textContent = '100'
+document.querySelector('.ball2').textContent = '100'
 
-var currentSize = 100 // מתחילים עם גובה התחלתי של 100px
+var currentSize1 = 100 // גובה התחלתי של הכדור הראשון
+var currentSize2 = 100 // גובה התחלתי של הכדור השני
 
-function onBallClick() {
-    var ball = document.querySelector('.ball')
+function onBallClick(ball) {
+    // יצירת מספר אקראי בין 20 ל-60
+    var increment = Math.floor(Math.random() * (60 - 20 + 1)) + 20
 
-     // יצירת מספר אקראי בין 20 ל-60
-     var increment = Math.floor(Math.random() * (60 - 20 + 1)) + 20
-     currentSize += increment
-    //  console.log('increment', increment)
+    // עדכון גובה הכדור בהתאם לכדור שנלחץ
+    if (ball.classList.contains('ball')) {
+        currentSize1 += increment
 
-    if (currentSize > 400) {
-        currentSize = 100 // reset to 100px
-        console.log('Resetting size to:', currentSize)
+        // מגבלה על גודל הכדור
+        if (currentSize1 > 400) {
+            currentSize1 = 100 // reset to 100px
+        }
+
+        // עדכון רוחב וגובה הכדור
+        ball.style.width = currentSize1 + 'px'
+        ball.style.height = currentSize1 + 'px'
+        ball.textContent = currentSize1; // עדכון הטקסט
+
+    } else if (ball.classList.contains('ball2')) {
+        currentSize2 += increment;
+
+        // מגבלה על גודל הכדור
+        if (currentSize2 > 400) {
+            currentSize2 = 100; // reset to 100px
+        }
+
+        // עדכון רוחב וגובה הכדור
+        ball.style.width = currentSize2 + 'px'
+        ball.style.height = currentSize2 + 'px'
+        ball.textContent = currentSize2 // עדכון הטקסט
     }
-
-    ball.style.width = currentSize+ 'px'
-    ball.style.height = currentSize + 'px'
-    ball.textContent = currentSize // עדכון הטקסט
-
 
     // שינוי צבע הכדור לצבע אקראי
     ball.style.backgroundColor = getRandomColor()
